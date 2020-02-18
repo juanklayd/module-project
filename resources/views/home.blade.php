cc4febe6f4774f7c90ae22d5c1e41edfd47fe9e8
faramere07 <faramere07@gmail.com>
2020-02-18 09:30:43 +0800

    Test UI: sample

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in! Enjoy! Test Test Test
+                    You are logged in! Enjoy! Test Test Test !
                 </div>
             </div>
         </div>
